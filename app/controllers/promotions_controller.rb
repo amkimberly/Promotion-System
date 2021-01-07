b622e1a9fb3d2fb9ccf3a2e9798da28118969c9a
Amanda Penido <kimberlypenido@gmail.com>
2021-01-07 12:21:08 -0300

    Criar Promoção

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -4,8 +4,26 @@ class PromotionsController < ApplicationController
     @promotions = Promotion.all
   end
 
+  def new
+    @promotion = Promotion.new
+  end
+
+  def create
+    @promotion = Promotion.create(promotion_params)
+    redirect_to @promotion
+  end
+
   def show
     @promotion = Promotion.find(params[:id])
   end
 
+
+  private
+
+  def promotion_params
+    params
+    .require(:promotion)
+    .permit(:name, :description, :code, :discount_rate,
+            :coupon_quantity, :expiration_date)
+  end
 end
